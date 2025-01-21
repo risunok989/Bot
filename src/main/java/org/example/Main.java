a3007e5d79aa724dc7f45aef1685eea523878450
admin <risunok989@gmail.com>
2025-01-21 23:02:43 +0300

    Реализовал регистрацию бота.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String botToken = "5384659247:AAG9CS7x4Dk_HRt6kDkzeuu9PWFRJAbhMDY";
+
+        // Экземпляр, для дальнейшей регистрации бота.
+        TelegramBotsLongPollingApplication application = new TelegramBotsLongPollingApplication();
+        // Регистрация бота.
+        try {
+            application.registerBot(botToken, new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
