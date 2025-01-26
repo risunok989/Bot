9534f2c91633d2ed13f2748619761455efb7ce86
admin <risunok989@gmail.com>
2025-01-26 22:56:40 +0300

    Добавил в класс SenderMessage метод для отправки фотографий.

--- src/main/java/org/example/MyAmazingBot.java
+++ src/main/java/org/example/MyAmazingBot.java
@@ -3,18 +3,22 @@ package org.example;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
 
 public class MyAmazingBot implements LongPollingSingleThreadUpdateConsumer {
     TelegramClient telegramClient = new OkHttpTelegramClient(new GetToken().token());
 //    SenderMessage senderMessage = new SenderMessage();
     private final Long ADMIN_ID = 249438024L;
     private final Long ALEX_ID = 6119250690L;
-    private final Long OLGA_ID = 6119250690L;
+    private final Long OLGA_ID = 645409728L;
 
     //Переопределил метод, т.к клас имплеминитрует зависимость.
     @Override
@@ -46,7 +50,27 @@ public class MyAmazingBot implements LongPollingSingleThreadUpdateConsumer {
 //            e.printStackTrace();
 //        }
         } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
-            System.out.println("Пользователем с id : " + update.getMessage().getChatId() + " прислал ФОТО.");
+            System.out.println("Пришло фото.");
+
+            Long chatId = update.getMessage().getChatId();
+            // Ложим в коллекцию фото разных размеров.
+            List<PhotoSize> photoSizes = update.getMessage().getPhoto();
+            // Выбираем качество изображения.
+            String f_id = photoSizes.stream().max(Comparator.comparing(PhotoSize::getFileSize))
+                    .map(PhotoSize::getFileId)
+                    .orElse("");
+            // Know photo width
+            int f_width = photoSizes.stream().max(Comparator.comparing(PhotoSize::getFileSize))
+                    .map(PhotoSize::getWidth)
+                    .orElse(0);
+            // Know photo height
+            int f_height = photoSizes.stream().max(Comparator.comparing(PhotoSize::getFileSize))
+                    .map(PhotoSize::getHeight)
+                    .orElse(0);
+            // Set photo caption
+            String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
+            InputFile inputFile = new InputFile(f_id);
+            new SenderMessage().sendPhotoMessage(chatId,inputFile, caption);
 
         }
     }
--- src/main/java/org/example/SenderMessage.java
+++ src/main/java/org/example/SenderMessage.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 // Наследуемся, для получения метода "TelegramClient telegramClient = new......."
 public class SenderMessage extends MyAmazingBot {
@@ -25,6 +26,22 @@ public class SenderMessage extends MyAmazingBot {
             System.err.println("Отправил эхо пользователю с ID : " + sendMessage.getChatId());
         } catch (TelegramApiException e) {
             throw new RuntimeException("Ошибка отправки сообщения", e);
+        }
+    }
+    public void sendPhotoMessage(Long chatId, InputFile photo, String caption) {
+        SendPhoto sendPhoto = SendPhoto.builder()
+                .chatId(chatId)
+                .photo(photo)
+                .caption("тест, мать       " + caption)
+                .build();
+        try {
+            try {
+                telegramClient.execute(sendPhoto);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException("Ошибка отправки фотографии", e);
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
     }
 
